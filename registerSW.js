9fb792a928d865c338d07b6ee682bd932d3dbebc
dnielpy <dnielpy@users.noreply.github.com>
2026-06-04 07:03:23 +0000

    deploy: clean-lps/Advanced_Plumbing_Services_INC@513c1b6bad81dbbfb6133f8113ab0e7b34d315a7

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/dnielpy-third-deploy/sw.js', { scope: '/dnielpy-third-deploy/' })})}
