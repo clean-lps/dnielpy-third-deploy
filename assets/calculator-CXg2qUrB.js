d2a4c627ca503fe7c2ed8a07afdccb91a0d8e6b2
dnielpy <dnielpy@users.noreply.github.com>
2026-06-18 23:55:48 +0000

    deploy: clean-lps/MARVIER-INSURANCE@1cc0f5b6ebc1975d5cfede8ff023d3bdc5bb0845

--- assets/calculator-CXg2qUrB.js
+++ assets/calculator-CXg2qUrB.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-0Uk9AyFH.js";/**
+import{c as e}from"./index-BM7krGCP.js";/**
  * @license lucide-react v0.480.0 - ISC
  *
  * This source code is licensed under the ISC license.
